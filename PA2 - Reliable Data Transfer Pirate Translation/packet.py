802e3e0fb29b37137e86eb785a54f34663729790
Benjamin Haedt <haedt.benjamin@gmail.com>
2022-10-19 17:19:47 -0600

    Update packet.py

--- PA2 - Reliable Data Transfer Pirate Translation/packet.py
+++ PA2 - Reliable Data Transfer Pirate Translation/packet.py
@@ -1,20 +1,23 @@
-from operator import length_hint
-import this
-
-
-class packet:
-    def __init__(seqNum, checkSum, ack_or_nak, length, message):
-        this.seqNum = seqNum
-        this.ack_or_nak = ack_or_nak
-        this.checkSum = checkSum   
-        this.length = length
-        this.message = message
-
+class packet():
+    def __init__(self, seqNum, checkSum, ack_or_nak, length, message):
+        self.seqNum = seqNum
+        self.ack_or_nak = ack_or_nak
+        self.checkSum = checkSum   
+        self.length = length
+        self.message = message
     def set_message(self,new_message):
         self.message = new_message
-    
+    def get_checkSum(self):
+        return self.checkSum
+    def get_AOK(self):
+        return self.ack_or_nak
+    def set_AON(self, AON):
+        self.ack_or_nak = AON
+    def get_len(self):
+        return self.length
     def get_message(self):
         return self.message
-
-    def get_sequence(self):
-        return self.sequence_number
+    def get_seqNum(self):
+        return self.seqNum    
+    def set_checkSum(self, checkSum):
+        self.checkSum = checkSum
