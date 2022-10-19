8623859b4ad63e7c1ae84ddd59429f537fed3b04
Benjamin Haedt <haedt.benjamin@gmail.com>
2022-10-19 17:19:00 -0600

    Update client.py

--- PA2 - Reliable Data Transfer Pirate Translation/client.py
+++ PA2 - Reliable Data Transfer Pirate Translation/client.py
@@ -3,32 +3,9 @@ import sys
 import socket
 import time
 import pickle
+import packet
 
-    
-class packet():
-    def __init__(self, seqNum, checkSum, ack_or_nak, length, message):
-        self.seqNum = seqNum
-        self.ack_or_nak = ack_or_nak
-        self.checkSum = checkSum   
-        self.length = length
-        self.message = message
-    def set_message(self,new_message):
-        self.message = new_message
-    def get_checkSum(self):
-        return self.checkSum
-    def get_AOK(self):
-        return self.ack_or_nak
-    def set_AON(self, AON):
-        self.ack_or_nak = AON
-    def get_len(self):
-        return self.length
-    def get_message(self):
-        return self.message
-    def get_seqNum(self):
-        return self.seqNum    
-    def set_checkSum(self, checkSum):
-        self.checkSum = checkSum
-
+packet = packet.packet
 def corruptOrNot(probability):
     return random.random() > probability
 
@@ -37,7 +14,7 @@ def recievedNAK(ob, prob, conn):
     ob = pickle.loads(ob)
     if(ob.get_AOK() == "NAK"):
         while (ob.get_AOK() == "NAK"):
-            print(("Corrupted Packer Recieved, Sending NAK, Sequence Number: ", ob.get_seqNum()))
+            print("Recieved NAK from sequence number: ", ob.get_seqNum())
             ob.set_checkSum(corruptOrNot(prob))
             ob = pickle.dumps(ob)
             time.sleep(1)
@@ -52,7 +29,7 @@ def corruptPacketChecker(ob, conn):
     if(ob.get_checkSum() != True):
         while (ob.get_checkSum() != True):
                 ob.set_AON("NAK")
-                print(("Corrupted Packer Recieved, Sending NAK, Sequence Number: ", ob.get_seqNum()))
+                print(("Corrupted Packet Recieved, Sending NAK, Sequence Number: ", ob.get_seqNum()))
                 ob = pickle.dumps(ob)
                 time.sleep(1)
                 conn.send(ob)
